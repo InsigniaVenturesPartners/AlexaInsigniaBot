520d00b696601f77238817969bd45cb74c6d7bd9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-27 13:49:46 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -61,8 +61,6 @@ class IntroductionIntentHandler(AbstractRequestHandler):
                 .response
         )
 
-
-
 class PlayIntentHandler(AbstractRequestHandler):
     
     def can_handle(self, handler_input):
