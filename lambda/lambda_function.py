4413917cd801e985cdaf0965e0b131450a3f2e7c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-27 14:24:27 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -270,6 +270,7 @@ sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(IntroductionIntentHandler())
+
 sb.add_request_handler(PlayIntentHandler())
 sb.add_request_handler(PauseIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
