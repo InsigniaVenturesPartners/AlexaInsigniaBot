ddb93eb1d4e034be5e57c760d6833ca59bfe32b3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-27 13:59:54 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -56,7 +56,7 @@ class IntroductionIntentHandler(AbstractRequestHandler):
         
         return (
             handler_input.response_builder
-                .speak(speak_output)
+                .speak(speak_output + "Here is a video for more information on Insignia Ventures Partners")
                 .speak()
                 .add_directive(video_directive)
                 .response
