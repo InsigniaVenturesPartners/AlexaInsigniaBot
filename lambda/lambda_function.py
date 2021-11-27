b55f415409949cbfccb1a89362809b6d0d8e892b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-27 14:17:26 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -88,7 +88,26 @@ class NoIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return ask_utils.is_intent_name("AMAZON.NoIntent")
     def handle(self, handler_input):
-        
+        speak_output = ""
+        if currentState == "PROMPTING_VIDEO":
+            video_directive = RenderDocumentDirective(
+                token = "VideoPlayer",
+                document = load_json_from_path("apl/render-videoplayer.json"),
+                datasources = create_all_video_playlist(playlist())
+            )
+            
+            return (
+                handler_input.response_builder
+                    .speak("Here is a video for more information on Insignia Ventures Partners")
+                    .add_directive(video_directive)
+                    .response
+            )
+        else:
+            return (
+                handler_input.response_builder
+                    .speak(speak_output)
+                    .response
+            )
 
 class PlayIntentHandler(AbstractRequestHandler):
     
