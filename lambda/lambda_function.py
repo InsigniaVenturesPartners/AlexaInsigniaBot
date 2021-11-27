de555fbf5f5ed0621f24f149f1b7763ca0cc46b0
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-27 14:11:04 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -69,7 +69,8 @@ class YesIntentHandler(AbstractRequestHandler):
         return ask_utils.is_intent_name("AMAZON.YesIntent")
     def handle(self, handler_input):
         speak_output = ""
-        if currentState == "PROMPTING_VIDEO"
+        if currentState == "PROMPTING_VIDEO":
+            
 
 class PlayIntentHandler(AbstractRequestHandler):
     
