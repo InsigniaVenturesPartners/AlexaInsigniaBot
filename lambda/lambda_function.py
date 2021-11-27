33c4b8461ec8ea8494ad8b029dc5411c2df73982
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-27 13:49:16 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -62,11 +62,7 @@ class VideoIntentHandler(AbstractRequestHandler):
         return ask_utils.is_intent_name("VideoIntent")(handler_input)
 
     def handle(self, handler_input):
-        video_directive = RenderDocumentDirective(
-            token = "VideoPlayer",
-            document = load_json_from_path("apl/render-videoplayer.json"),
-            datasources = create_all_video_playlist(playlist())
-        )
+        
         return (
             handler_input.response_builder
                 .speak("Here is a video for more information on Insignia Ventures Partners")
