d4845ab96026ecdc53f207fda9729a78788cec31
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-29 07:16:50 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -38,7 +38,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        print("hello")
         speak_output = "Welcome, my name is Insignia Bot, I'm here to tell you more about Insignia Ventures Partners"
 
         return (
