802200bd66272d2c32fbe223236a22db232d76c6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-29 16:41:37 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -142,6 +142,8 @@ class InvestorInfoIntentHandler(AbstractRequestHandler):
                 .response
         )
 
+
+
 class YesIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return ask_utils.is_intent_name("AMAZON.YesIntent")(handler_input)
