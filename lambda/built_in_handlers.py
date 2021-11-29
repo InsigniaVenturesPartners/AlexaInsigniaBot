e77720671bb9a634911921839e0f2c01718f855a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-29 16:45:33 +0000

    updated lambda/built_in_handlers.py.

--- lambda/built_in_handlers.py
+++ lambda/built_in_handlers.py
@@ -1,4 +1,3 @@
-from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
