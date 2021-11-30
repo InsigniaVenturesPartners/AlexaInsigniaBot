3d4419a4d2b5fd63e73179fb41d0b2a5281965b6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-30 06:55:52 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -31,7 +31,7 @@ def get_video_directive():
     video_directive = RenderDocumentDirective(
         token = "VideoPlayer",
         document = load_json_from_path("apl/render-videoplayer.json"),
-        datasources = {}
+        datasources = playlist()
     )
     return video_directive
 
