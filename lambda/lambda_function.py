d3c01de31331cf96c249cc5bb66eec67f34607e9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-30 06:55:29 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -21,7 +21,7 @@ DATA = load_json_from_path("data.json")
 def playlist():
     return [
         {
-            "url": create_presigned_url("Media/INSIGNIA_VC_VIDEO.mp4"),
+            "videoURL": create_presigned_url("Media/INSIGNIA_VC_VIDEO.mp4"),
             "title": "Insignia Video",
             "subtitle": ""
         }    
