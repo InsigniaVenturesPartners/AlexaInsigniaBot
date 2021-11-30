71c084b77a707beb48cd157c000184878e21729c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-30 07:34:57 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -27,18 +27,7 @@ def get_video_directive():
         document = load_json_from_path("apl/render-videoplayer.json"),
         datasources = {
             {
-                "videoplayerData": {
-                    "type": "object",
-                    "properties": {
-                        "playlist": [
-                            {
-                            "url": create_presigned_url("Media/INSIGNIA_VC_VIDEO.mp4"),
-                            "title": "Insignia Video",
-                            "subtitle": ""
-                            }
-                        ]
-                    }
-                }
+                
             }
         }
     )
