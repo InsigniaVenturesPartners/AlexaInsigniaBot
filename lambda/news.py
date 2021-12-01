e00a23e9fb4c1912dd9aa4d4dce22c1291767a5b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-01 12:11:35 +0000

    updated lambda/news.py.

--- lambda/news.py
+++ lambda/news.py
@@ -1,33 +1,27 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import requests
+import datetime
+
+URL = "https://www.insignia.vc/startup_info/data.json"
 
 def get_news():
-    driver = webdriver.Chrome() # might need to set up some stuff for lambda
-    URL = "https://www.insignia.vc/"
+    response = requests.get(URL)
+    response_json = response.json()
 
-    driver.get(URL)
-    source = driver.page_source
-    driver.close()
+    companies = response_json["portfolios"]
+    data = [company["public_info"] for company in companies]
+    articles = [
+        {
+            "title": item["external_article_title"],
+            "source": item["external_article_source"],
+            "url": item["external_article_link"],
+            "date": item["external_article_date"],
+        }
+        for item in data
+    ]
 
-    soup = BeautifulSoup(source, "html.parser")
+    articles.sort(
+        key=lambda article: datetime.datetime.strptime(article["date"] or "1970-01-01","%Y-%m-%d"),
+        reverse=True
+    )
 
-    data = ""
-
-    for i in soup.select(".py-5.container"):
-        if "LATEST NEWS" in i.text: # latest news div
-            data = i
-
-    result = []
-
-    for i in data.children:
-        if not i.get("class"): # the news divs don't have classes for some reason
-            children = i.findChildren("a", recursive=True)
-            print(children[0],children[1])
-            article = {
-                "title": children[0].contents[0],
-                "url": children[0]["href"],
-                "publisher": children[1].contents[0],
-            }
-            result.append(article)
-
-    return result
+    return articles
