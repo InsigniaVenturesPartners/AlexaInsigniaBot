62cd7bc67456d515174fc0be2ad058d8af2bff85
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-05 07:47:16 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -168,7 +168,8 @@ class VideoIntentHandler(AbstractRequestHandler):
         return ask_utils.is_intent_name("VideoIntent")(handler_input)
     
     def handle(self, handler_input):
-        
+        if get_supported_interfaces(
+                handler_input).alexa_presentation_apl is not None:
         
         video_directive = get_video_directive()
         return (
