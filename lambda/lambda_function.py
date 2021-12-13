5d0240e54e14229b4f9900aee33388bd8ca3e89e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-13 07:24:55 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -25,15 +25,7 @@ def get_coinvestor(coinvestor):
 
 def get_video_directive():
     video_directive = RenderDocumentDirective(
-        token = "VideoPlayer",
-        document = ,
-        datasources = {
-            "videoPlayerData": {
-                "properties": {
-                    "url": create_presigned_url("Media/INSIGNIA_VC_VIDEO.mp4")
-                }
-            }
-        }
+        load_json_from_path("apl/videoplayer.json")
     )
     return video_directive
 
