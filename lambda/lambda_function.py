a8081e8b8fa7178e83be9258ca8b77c59d8aac00
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-14 03:51:09 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -84,9 +84,11 @@ class NewsIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return ask_utils.is_intent_name("NewsIntent")(handler_input)
 
-    def handle(self, handler_input): 
+    def handle(self, handler_input):
         speech_output = get_news()[READ_NEWS]["title"]
         READ_NEWS += 1
+        
+        
         return (
             handler_input.response_builder
                 .speak(speech_output)
