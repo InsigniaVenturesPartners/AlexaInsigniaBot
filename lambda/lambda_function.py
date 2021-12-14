7d898e29d3e8c1f85358ae2d3a363758f012a1cb
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-14 03:35:32 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -84,8 +84,12 @@ class NewsIntentHandler(AbstractRequestHandler):
         return ask_utils.is_intent_name("NewsIntent")(handler_input)
 
     def handle(self, handler_input): 
-        news = [i["title"] + ". " for i in get_news()[:2]]
-        speech_output = "".join(news)
+        speech_output = ""
+        for i, news in enumerate(get_news()[:2]):
+            speech_output += f"{i}. {news}. "
+        
+        # news = [i["title"] + ". " for i in get_news()[:2]]
+        # speech_output = "".join(news)
         print(speech_output)
         return (
             handler_input.response_builder
