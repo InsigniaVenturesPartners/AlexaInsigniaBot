1c21d920cb25dbee0a44f3a432dec9a526745faf
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-14 03:37:00 +0000

    updated lambda/news.py.

--- lambda/news.py
+++ lambda/news.py
@@ -8,16 +8,26 @@ def get_news():
     response_json = response.json()
 
     companies = response_json["portfolios"]
-    data = [company["public_info"] for company in companies]
-    articles = [
-        {
-            "title": item["external_article_title"],
-            "source": item["external_article_source"],
-            "url": item["external_article_link"],
-            "date": item["external_article_date"],
-        }
-        for item in data
-    ]
+    articles = []
+    for company in companies:
+        articles.append(
+            {
+                "title": company["public_info"]["external_article_title"],
+                "source": company["public_info"]["external_article_source"],
+                "url": company["public_info"]["external_article_link"],
+                "date": company["public_info"]["external_article_date"],
+            }
+        )
+    # data = [company["public_info"] for company in companies]
+    # articles = [
+    #     {
+    #         "title": item["external_article_title"],
+    #         "source": item["external_article_source"],
+    #         "url": item["external_article_link"],
+    #         "date": item["external_article_date"],
+    #     }
+    #     for item in data
+    # ]
 
     articles.sort(
         key=lambda article: datetime.datetime.strptime(article["date"] or "1970-01-01","%Y-%m-%d"),
