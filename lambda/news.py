11d202c173260ff3fdb4a462e0d51b295428e7d3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-14 03:58:09 +0000

    updated lambda/news.py.

--- lambda/news.py
+++ lambda/news.py
@@ -18,16 +18,6 @@ def get_news():
                 "date": company["public_info"]["external_article_date"],
             }
         )
-    # data = [company["public_info"] for company in companies]
-    # articles = [
-    #     {
-    #         "title": item["external_article_title"],
-    #         "source": item["external_article_source"],
-    #         "url": item["external_article_link"],
-    #         "date": item["external_article_date"],
-    #     }
-    #     for item in data
-    # ]
 
     articles.sort(
         key=lambda article: datetime.datetime.strptime(article["date"] or "1970-01-01","%Y-%m-%d"),
